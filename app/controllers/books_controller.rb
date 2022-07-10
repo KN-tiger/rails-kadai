c0190a5ed2a6610bdf610c66581cffdd276b9572
KN-tiger <108563821+KN-tiger@users.noreply.github.com>
2022-07-10 07:25:24 +0000

    [add]index

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,29 +5,35 @@ class BooksController < ApplicationController
 
   def create
 
-    book = Book.new(book_params)
+    @book = Book.new(book_params)
 
-    book.save
-    redirect_to '/books/new'
-
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def index
+    @book = Book.new
     @books = Book.all
   end
 
   def show
-     @book = Book.find(params[:id])
+    @book = Book.find(params[:id])
 
   end
 
   def edit
   end
 
-   private
+  private
 
   def book_params
     params.require(:book).permit(:title, :body)
   end
+
+
 
 end
